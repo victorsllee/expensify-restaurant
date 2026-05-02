0c15f0f016b540b1cce6666252da18a55eb87a79
victorsllee <victorsllee@gmail.com>
2026-05-02 23:23:25 +0700

    fix: Correctly set favicon based on environment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,26 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: `public/favicon${mode === 'staging' ? '-staging' : mode === 'development' ? '-dev' : ''}.svg`,
+          dest: '.',
+          rename: 'favicon.svg'
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
